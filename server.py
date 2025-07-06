86fe93d08f4ffed37e4f7f06e13d73ad2d438c2d
Lavanya-4321 <116573812+Lavanya-4321@users.noreply.github.com>
2025-07-06 17:24:33 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import util
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/classify_image', methods=['GET', 'POST'])
+@app.route('/classify_image', methods=['POST'])
 def classify_image():
-    image_data = request.form['image_data']
+    image_data = request.files['image_data'].read()  # updated for file input
+    result = util.classify_image(image_data)
 
-    response = jsonify(util.classify_image(image_data))
-
-    response.headers.add('Access-Control-Allow-Origin', '*')
-
-    return response
+    return jsonify(result)
 
 if __name__ == "__main__":
-    print("Starting Python Flask Server For Sports Celebrity Image Classification")
+    print("Starting Flask Server...")
     util.load_saved_artifacts()
     app.run(port=5000)
